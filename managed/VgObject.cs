81b491316a5819ded9500e4e5ea331644b73250d
Alexander Klingenbeck <alexander.klingenbeck@gmx.de>
2021-10-31 17:29:56 +0100

    remove false header

--- managed/VgObject.cs
+++ managed/VgObject.cs
@@ -1,8 +1,4 @@
-﻿/* -------------------------------------------------------------------------------------------------
-   Restricted - Copyright (C) Siemens Healthcare GmbH/Siemens Medical Solutions USA, Inc., 2019. All rights reserved
-   ------------------------------------------------------------------------------------------------- */
-   
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
